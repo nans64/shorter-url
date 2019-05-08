2a05fd1f56c11dd74abea5bfab5bd779fc270626
nans64 <nans.noel@gmail.com>
2019-05-08 23:08:25 +0200

    Fix a bug due to "/"

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -5,7 +5,8 @@ class ShortenedUrl < ApplicationRecord
     url.strip!
     url = url.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
     url.slice!(0) if url[0] == "/"
-    url = "http://#{url}/"
+    url.slice!(-1) if url[-1] == "/"
+    url = "http://#{url}"
   end
 
 end
