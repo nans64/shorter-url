fd462c1cb9e3c54e1ab591cfe9f7b56bf767a49b
nans64 <nans.noel@gmail.com>
2019-05-08 15:38:57 +0200

    Add the back index page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,26 @@
 class StaticPagesController < ApplicationController
   def index
+    @urls = Shortener::ShortenedUrl.all
 
-    if params[:search] ? Shortener::ShortenedUrl.generate(params[:search], owner: current_user) : nil
+    unless params[:url].nil?
+
+      if Shortener::ShortenedUrl.where(url: params[:url], owner: current_user).empty?
+        Shortener::ShortenedUrl.generate(params[:url], owner: current_user, fresh: true)
+        flash[:success] = "L'URL a été transformé avec succès et attaché à votre compte."
+
+      elsif !Shortener::ShortenedUrl.where(url: params[:url]).empty?
+
+        @urls = Shortener::ShortenedUrl.where(url: params[:url])
+        flash[:success] = "L'URL a déjà été ajouté, vous pourrez la trouver ci-dessous."
+
+      else
+
+        Shortener::ShortenedUrl.generate(params[:url], owner: current_user)
+        flash[:success] = "L'URL a été transformé avec succès."
+        redirect_to "/"
+
+      end
+
     end
-
   end
-
 end
