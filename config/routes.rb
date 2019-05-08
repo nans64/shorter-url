c62abfc52a2a416d691a53c55bfd11efeeb59d29
nans64 <nans.noel@gmail.com>
2019-05-08 12:01:41 +0200

    Add devise + Front

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  root to: "static_pages#index"
 end
