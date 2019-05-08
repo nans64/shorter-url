055a5cfbc905e55ff01a6a638c3979f906b1fdcf
nans64 <nans.noel@gmail.com>
2019-05-08 22:50:05 +0200

    add the domain to the table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,17 +9,17 @@ class StaticPagesController < ApplicationController
       if Shortener::ShortenedUrl.where(url: @url, owner: current_user).empty?
 
         Shortener::ShortenedUrl.generate(@url, owner: current_user, fresh: true)
-        flash[:success] = "L'URL a été transformé avec succès par la méthode 1."
+        flash[:success] = "L'URL a été transformé avec succès - 1."
 
       elsif !Shortener::ShortenedUrl.where(url: @url).empty?
 
         @urls = Shortener::ShortenedUrl.where(url: @url)
-        flash[:success] = "L'URL a déjà été ajouté, vous pourrez la trouver ci-dessous par la méthode 2"
+        flash[:success] = "L'URL a déjà été ajouté, vous pourrez la trouver ci-dessous."
 
       else
 
         Shortener::ShortenedUrl.generate(@url, owner: current_user)
-        flash[:success] = "L'URL a été transformé avec succès par la méthode 3."
+        flash[:success] = "L'URL a été transformé avec succès - 3."
         redirect_to "/"
 
       end
