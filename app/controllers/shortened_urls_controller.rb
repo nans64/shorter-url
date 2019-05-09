14e73745ec7eaa2b7159e56dd33fa6b3dd87a771
nans64 <nans.noel@gmail.com>
2019-05-10 00:04:28 +0200

    Refactor the input to integrate it into the create + reintegrate the result

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,61 +1,53 @@
-class StaticPagesController < ApplicationController
+class ShortenedUrlsController < ApplicationController
     before_action :set_url, only: [:edit, :update, :destroy]
 
     def index
       @urls = ShortenedUrl.all
-      @url = ShortenedUrl.new
     end
 
     # GET /videos/new
     def new
+      @url = ShortenedUrl.new
     end
-
-    def show
-    end
-
 
     def edit
     end
 
     def create
 
-      if ShortenedUrl.is_not_empty?(params[:url])
+      if ShortenedUrl.is_not_empty?(url_params[:url])
 
-        @url = ShortenedUrl.sanitize(params[:url])
+        @url = ShortenedUrl.sanitize(url_params[:url])
 
-        if ShortenedUrl.where(url: @url, owner: current_user).empty?
+        if ShortenedUrl.where(url: @url, owner_id: current_user).empty?
 
           Shortener::ShortenedUrl.generate(@url, owner: current_user, fresh: true, expires_at: 72.hours.since)
+          flash[:success] = "L'URL a été transformé avec succès - 1."
+          redirect_to(root_path)
 
         elsif !ShortenedUrl.where(url: @url).empty?
 
+          @urls = ShortenedUrl.where(url: @url)
+          flash[:success] = "L'URL a déjà été ajouté, vous pourrez la trouver ci-dessous."
+
+          redirect_to static_pages_path(url: @urls.last.id) # ne fonctionne pas
         else
 
           Shortener::ShortenedUrl.generate(@url, owner: current_user, expires_at: 72.hours.since)
-
+          flash[:success] = "L'URL a été transformé avec succès - 3."
+          redirect_to(root_path)
         end
-
       end
-
-      if @url.save
-        redirect_to(root_path)
-        flash[:success] = "L'URL a été transformé avec succès."
-      else
-        redirect_to(root_path)
-        @urls = ShortenedUrl.where(url: @url)
-        flash[:alert] = "L'URL a déjà été ajouté, vous pourrez la trouver ci-dessous."
-      end
-
     end
 
     def update
-
-      if params[:id] && params[:nombre]
-        ShortenedUrl.add_days(params[:id], params[:nombre])
+      byebug
+      if url_params[:id] && url_params[:nombre]
+        ShortenedUrl.add_days(url_params[:id], url_params[:nombre])
       end
 
-      if params[:id] && params[:date]
-        ShortenedUrl.expire(params[:id], params[:date])
+      if url_params[:id] && url_params[:date]
+        ShortenedUrl.expire(url_params[:id], url_params[:date])
       end
 
       respond_to do |format|
